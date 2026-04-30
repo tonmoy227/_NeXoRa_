62b96c0e20a0e059a512f2cb79398e37799547e7
tonmoy227 <145573783+tonmoy227@users.noreply.github.com>
2026-04-30 18:46:31 +0600

    5555
    555

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,6 +117,15 @@ Last change:    00/00/00
 		pauseOnHover: true,
 		startVisible:true,
 	});
+	$('.marquee-left2').marquee({
+		gap: 0,
+		speed: 40,
+		delayBeforeStart: 0,
+		direction: 'left',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	});
 	$('.marquee-right').marquee({
 		gap: 0,
 		speed: 10,
@@ -137,6 +146,24 @@ Last change:    00/00/00
 		startVisible:true,
 	});
 
+	$('.marquee-right3').marquee({
+		gap: 0,
+		speed: 40,
+		delayBeforeStart: 0,
+		direction: 'right',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	});
+	$('.marquee-right4').marquee({
+		gap: 0,
+		speed: 40,
+		delayBeforeStart: 0,
+		direction: 'right',
+		duplicated: true,
+		pauseOnHover: true,
+		startVisible:true,
+	});
 	// windows-loaded-before-functions
 	document.addEventListener("DOMContentLoaded", function () {
 		window.addEventListener('load', function(){
@@ -215,18 +242,236 @@ Last change:    00/00/00
 			},
 		});
 	}
+	if (window.matchMedia("(min-width: 1600px)").matches) {
+		const listItems = document.querySelectorAll(".nxr-service-list-wrap li");
+		const cards = Array.from(document.querySelectorAll(".nxr-ser-card-item"));
 
-	var swiper = new Swiper(".nxr-service-card-wrapper", {
-		effect: "cards",
-		grabCursor: true,
+		listItems.forEach((li, index) => {
+			li.addEventListener("click", () => {
 
-		cardsEffect: {
-			perSlideOffset: 10,
-			perSlideRotate: 5, 
-			rotate: true,
-			slideShadows: false,
-		}
+
+				listItems.forEach(el => el.classList.remove("active"));
+				li.classList.add("active");
+
+
+				const reordered = [
+					...cards.slice(index),
+					...cards.slice(0, index)
+				];
+
+
+				cards.forEach(card => {
+					card.classList.remove(
+						"ver_1",
+						"ver_2",
+						"ver_3",
+						"ver_4",
+						"ver_5",
+						"active"
+						);
+				});
+
+
+				reordered.forEach((card, i) => {
+					card.classList.add(`ver_${i + 1}`);
+
+
+					if (i === 0) {
+						card.classList.add("active");
+					}
+				});
+
+			});
+		});
+	};
+
+	if($(".bottom-text").length) {
+		var aniTitle1 = $(".bottom-text");
+		if(aniTitle1.length == 0) return; gsap.registerPlugin(SplitText); aniTitle1.each(function(index, el) {
+
+			el.split = new SplitText(el, { 
+				type: "lines,words,chars",
+				linesClass: "split-line"
+			});
+
+			gsap.set(el, { perspective: 400 });
+
+			if( $(el).hasClass('bottom-text') ){
+				gsap.set(el.split.chars, {
+					yPercent: 200,
+					opacity: 0,
+
+				});
+			}
+			el.anim = gsap.to(el.split.chars, {
+				scrollTrigger: {
+					trigger: el,
+					start: "top 90%",
+					toggleActions: "play reverse play reverse",
+					markers: false,
+
+				},
+
+				yPercent: 0,
+				xPercent: 0,
+				opacity: 1,
+				duration: 3,
+				stagger: .2,
+				ease: "bounce.out",
+			});
+
+		});
+	}
+
+
+	if (window.matchMedia("(min-width: 1200px)").matches) { 
+		document.querySelectorAll('.nxr-portfolio-sec').forEach((section) => {
+			let SecTitle = gsap.timeline({
+				scrollTrigger: {
+					trigger: section,
+					start: "top 10%",
+					end: "+=2000",
+					scrub: 1,
+					pin: false,
+					pinSpacing: false,
+					markers: false,
+				}
+			});
+
+			SecTitle.to(section.querySelector('.nxr-port-big'), { y: 1500,   scale: 2, duration: 1, ease: "power2.out"});
+		});
+	}
+
+
+	if (window.matchMedia("(min-width: 1200px)").matches) { 
+		var nxWC2 = gsap.timeline({
+			scrollTrigger: {
+				trigger: '.nxr-team-content-wrap',
+				start: "top 0%",
+				end: "bottom +=50",
+				markers: false,
+				scrub: 4,
+			}
+
+		});
+		nxWC2
+		.from(".nxr-team-text-wrap .subtitle", { y: 45, duration: .5,   ease: "power1.out" })
+		.from(".nxr-team-title .line_1", { y: 45, duration: .5,   ease: "power1.out" },"<")
+		.from(".nxr-team-title .line_2", {   x: -45, duration: .5,   ease: "power1.out" },"<")
+		.from(".nxr-team-title .line_3", {   y: -45, duration: .5,   ease: "power1.out" },"<")
+		.from(".nxr-team-feed-wrap", { height: 0, duration: 1,   ease: "power1.out" },"< = .1")
+		.from(".nxr-team-item._3", { scale: 1.1,   ease: "power1.out" },"< = .5")
+		.from(".nxr-team-item._2", { xPercent: 105,   ease: "power1.out" },"< = .1")
+		.from(".nxr-team-item._4", { xPercent: -105,   ease: "power1.out" },"< =")
+		.from(".nxr-team-item._1", { xPercent: 215,   ease: "power1.out" },"< = .1")
+		.from(".nxr-team-item._5", { xPercent: -215,   ease: "power1.out" },"< = ")
+	};
+
+	var quick_view = new Swiper(".nxr-testi-thumb-slider", {
+		loop: true,
+		spaceBetween: 10,
+		slidesPerView: 7,
+		direction: "vertical",
+		centeredSlides: true,
+		watchSlidesProgress: true,
+		watchSlidesVisibility: true,
+		slideToClickedSlide: true,
+
+		navigation: {
+			prevEl: ".views-button-prev",
+			nextEl: ".views-button-next",
+		},
+
+		breakpoints: {
+			1400: {
+				slidesPerView: 7,
+				direction: "vertical",
+			},
+			1200: {
+				slidesPerView: 7,
+				direction: "horizontal",
+			},
+			1024: {
+				slidesPerView: 4,
+				direction: "horizontal",
+			},
+			991: {
+				slidesPerView: 3,
+				direction: "horizontal",
+			},
+			768: {
+				slidesPerView: 3,
+				direction: "horizontal",
+			},
+			577: {
+				slidesPerView: 3,
+				direction: "horizontal",
+			},
+			0: {
+				slidesPerView: 3,
+				direction: "horizontal",
+			},
+		},
+
+		on: {
+			slideChange: function () {
+
+				$('.nxr-testi-thumb-slider .swiper-slide')
+				.removeClass(
+					'swiper-slide-nth-prev-2 swiper-slide-nth-next-2 swiper-slide-nth-prev-3 swiper-slide-nth-next-3'
+					);
+				let activeSlide = $(this.slides[this.activeIndex]);
+				activeSlide.prev().prev().addClass('swiper-slide-nth-prev-2');
+				activeSlide.next().next().addClass('swiper-slide-nth-next-2'); 
+				activeSlide.prev().prev().prev().addClass('swiper-slide-nth-prev-3');
+				activeSlide.next().next().next().addClass('swiper-slide-nth-next-3');
+			},
+		},
 	});
+	var swiper2 = new Swiper(".nxr-testimonial-slider", {
+		loop: true,
+		spaceBetween: 0,
+		speed: 1000,
+		slidesPerView: 1,
+
+		navigation: {
+			prevEl: ".views-button-prev",
+			nextEl: ".views-button-next",
+		},
+
+		thumbs: {
+			swiper: quick_view,
+		},
+	});
+
+
+	if (window.matchMedia("(min-width: 1200px)").matches) {
+		gsap.to(".nxr-work-pro-title", {
+			scrollTrigger: {
+				trigger: ".nxr-work-process-sec",
+				start: "top 0%", 
+				end: "bottom bottom", 
+				pin: ".nxr-work-pro-title", 
+				pinSpacing: false,
+				markers: false
+			}
+		});
+
+		let proTitle = gsap.timeline({
+			scrollTrigger: {
+				trigger: ".nxr-work-process-sec",
+				start: "top 20%", 
+				end: "top -30%",
+				toggleActions: "play none none reverse",
+				scrub: true,
+				markers: false,
+			},
+		});
+
+		proTitle.to(".nxr-work-pro-title .nxr-wrp-left-title", { xPercent: -85});
+		proTitle.to(".nxr-work-pro-title .nxr-wrp-right-title", { xPercent: 85},"<");
+	}
+
 
 
 
